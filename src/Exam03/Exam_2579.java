df7b226be69f037065bf463fa619d6402e113489
hwan <hahaha1719@naver.com>
2024-06-21 17:52:28 +0900

    06/21 Exam Commit

---
+++ src/Exam03/Exam_2579.java
@@ -0,0 +1,33 @@
+package Exam03;
+
+import java.util.Scanner;
+
+public class Exam_2579 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] stairs = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            stairs[i] = sc.nextInt();
+        }
+
+        if (n == 1) {
+            System.out.println(stairs[1]);
+            return;
+        }
+
+        dp[1] = stairs[1];
+        if (n > 1) {
+            dp[2] = stairs[1] + stairs[2];
+        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + stairs[i - 1]) + stairs[i];
+        }
+
+        System.out.println(dp[n]);
+    }
+}
