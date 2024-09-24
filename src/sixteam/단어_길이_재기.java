e90a16c3109110cbacdf9c899ccd7e7c924a0cd0
hwan <hahaha1719@naver.com>
2024-09-24 21:33:43 +0900

    09/24 Exam Commit

---
+++ src/sixteam/단어_길이_재기.java
@@ -0,0 +1,15 @@
+package sixteam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 단어_길이_재기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String line = br.readLine();
+
+        System.out.println(line.length());
+    }
+}
---
+++ src/sixteam/문자열_분석.java
@@ -0,0 +1,34 @@
+package sixteam;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 문자열_분석 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            int lower = 0;
+            int upper = 0;
+            int digit = 0;
+            int space = 0;
+
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+                if (Character.isLowerCase(c)) {
+                    lower++;
+                } else if (Character.isUpperCase(c)) {
+                    upper++;
+                } else if (Character.isDigit(c)) {
+                    digit++;
+                } else if (c == ' ') {
+                    space++;
+                }
+            }
+
+            System.out.println(lower + " " + upper + " " + digit + " " + space);
+        }
+    }
+}
