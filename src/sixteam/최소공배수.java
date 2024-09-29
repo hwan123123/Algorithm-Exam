dea667b51b2b2689c70ad1ccd396f9f511841cfb
hwan <hahaha1719@naver.com>
2024-09-29 17:25:10 +0900

    09/27 Exam Commit

---
+++ src/sixteam/최소공배수.java
@@ -0,0 +1,37 @@
+package sixteam;
+
+import java.util.Scanner;
+
+public class 최소공배수 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            // GCD 계산
+            int gcd = gcd(A, B);
+
+            // LCM 계산
+            long lcm = (long) A * B / gcd;
+
+            sb.append(lcm).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    // 유클리드 호제법
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+}
