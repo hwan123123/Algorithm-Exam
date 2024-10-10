fd19fe15ae9d0f0cfb68116ff11ce3993fe73787
hwan <hahaha1719@naver.com>
2024-10-10 15:39:52 +0900

    10/09 Exam Commit

--- src/sixteam/일이삼더하기_5.java
+++ src/sixteam/일이삼더하기_5.java
@@ -1,38 +1,39 @@
 package sixteam;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class 일이삼더하기_5 {
-    public static final int MOD = 1000000009;
+    static final int MOD = 1000000009;
+    static long[][] dp = new long[100001][4];
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        dp[1][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i <= 100000; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % MOD;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % MOD;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % MOD;
+        }
+
         int T = Integer.parseInt(br.readLine());
 
-        int maxN = 100000;
-        long[][] dp = new long[maxN + 1][4]; // dp[i][j] -> i를 만들 때 마지막으로 사용한 숫자가 j인 경우의 방법 수
-
-        // 초기값 설정
-        if (maxN >= 1) dp[1][1] = 1; // 1을 만들 때 마지막 숫자가 1인 경우
-        if (maxN >= 2) dp[2][2] = 1; // 2를 만들 때 마지막 숫자가 2인 경우
-        if (maxN >= 3) dp[3][1] = 1; dp[3][2] = 1; dp[3][3] = 1; // 3을 만들 때 마지막 숫자가 1, 2, 3인 경우 각각
-
-        // 점화식
-        for (int i = 4; i <= maxN; i++) {
-            dp[i][1] = (dp[i-2][2] + dp[i-3][3]) % MOD;
-            dp[i][2] = (dp[i-1][1] + dp[i-3][3]) % MOD;
-            dp[i][3] = (dp[i-1][1] + dp[i-2][2]) % MOD;
-        }
-
-        StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
             int n = Integer.parseInt(br.readLine());
-            long result = (dp[n][1] + dp[n][2] + dp[n][3]) % MOD;
-            sb.append(result).append("\n");
+            long res = (dp[n][1] + dp[n][2] + dp[n][3]) % MOD;
+            bw.write(res + "\n");
         }
 
-        System.out.print(sb);
+        bw.flush();
     }
 }
